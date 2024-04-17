05717c5c689433c29ad660bc7318bbd269fc10ef
mitya <8863314@gmail.com>
2024-04-17 16:56:14 +0300

    PowerShell 7 profile

---
+++ windows/profile.ps1
@@ -0,0 +1,53 @@
+Write-Output "cgsg forever!"
+
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\robbyrussell.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\lightgreen.omp.json" | Invoke-Expression
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\cinnamon.omp.json" | Invoke-Expression
+
+Set-Alias winfetch pwshfetch-test-1
+
+Remove-Alias ls
+Set-Alias ls lsd
+function La-Function { lsd -a }
+Set-Alias la La-Function
+function Ll-Function { lsd -l }
+Set-Alias ll Ll-Function
+
+if (-Not (test-Path -Path "Z:\")) {
+  cd C:\code\
+  ./setZ.bat
+}
+cd Z:\
+
+${function:~} = { Set-Location ~ }
+function Cd-Back-Function { Set-Location .\.. }
+Set-Alias .. Cd-Back-Function
+${function:...} = { Set-Location ..\.. }
+${function:....} = { Set-Location ..\..\.. }
+${function:.....} = { Set-Location ..\..\..\.. }
+${function:......} = { Set-Location ..\..\..\..\.. }
+
+${function:docs} = { Set-Location ~\Documents }
+${function:dwld} = { Set-Location ~\Downloads }
+
+function Mk-Fall-Function([string] $path) { 
+  New-Item $path -ItemType Directory -ErrorAction SilentlyContinue; Set-Location $path
+}
+Set-Alias mkfall Mk-Fall-Function
+
+function Reboot-Function { shutdown -r -t 0 }
+Set-Alias reboot Reboot-Function
+function Poweroff-Function { shutdown -s -t 0 }
+Set-Alias poweroff Poweroff-Function
+
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+
+function History-Function {
+   $historyFilePath = (Get-PSReadlineOption).HistorySavePath
+   Get-Content -Path $historyFilePath 
+}
+Set-Alias history History-Function
+
+Set-Alias grep Select-String
+Set-Alias touch ni
